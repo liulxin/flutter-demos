c57308ba3586dabd6fe140663f7af24c5697a4c7
liulxin <550293874@qq.com>
2020-03-27 14:20:41 +0800

    first pro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,14 +9,55 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return new MaterialApp(
       title: 'Welcome to Fultter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome'),
-        ),
-        body: new Center(
-          child: new Text('Hello World'),
-        ),
+      home: new RandomWords()
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  // 生成列表
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  // 单个单词项
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+     return new Scaffold(
+       appBar: new AppBar(
+         title:new Text('Startup Name Generator') ,
+       ),
+       body: _buildSuggestions(),
+     );
   }
 }
