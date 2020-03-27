48853355bbcba67c49b6293ce88f4bbbbe776a04
liulxin <550293874@qq.com>
2020-03-27 13:17:30 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: 'Welcome to Fultter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome'),
+        ),
+        body: new Center(
+          child: new Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
