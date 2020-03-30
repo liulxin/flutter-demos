1b19a3dab9a7255b00b4e6a771e8c457574ec7d4
liulxin <550293874@qq.com>
2020-03-30 19:42:42 +0800

    route

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final title = 'Horizontal List';
 
-    return new MaterialApp(
+    return MaterialApp(
       title: title,
-      home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text(title),
-          ),
-          body: new Center(
-            child: new Card(
-              color: Color.fromARGB(6, 55, 44, 20),
-              child: new Column(
-                children: <Widget>[
-                  new ListTile(
-                    leading: new Icon(Icons.phone),
-                    title: new Text('电话'),
-                  ),
-                  new Divider(),
-                  new ListTile(
-                    leading: new Icon(Icons.add),
-                    title: new Text('添加'),
-                  ),
-                  new Divider(),
-                  new ListTile(
-                    leading: new Icon(Icons.remove),
-                    title: new Text('移除'),
-                  ),
-                  new Divider(),
-                  new ListTile(
-                    leading: new Icon(Icons.train),
-                    title: new Text('行程'),
-                  ),
-                ],
-              ),
-            ),
-          )),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Parent(),
+      ),
+    );
+  }
+}
+
+// 使用 Navigator 不能在根组件MaterialApp 中使用
+class Parent extends StatelessWidget {
+  final prods = List.generate(10, (index) => 'item $index');
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView.builder(
+        itemCount: prods.length,
+        itemBuilder: (context, i) {
+          return RaisedButton(
+            color: Colors.lightGreen,
+            child: Text('跳转 ${prods[i]}'),
+            onPressed: () async {
+              final res = await Navigator.of(context).push(
+                new MaterialPageRoute(
+                    builder: (context) => Child(pro: prods[i])),
+              );
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('$res'),
+              ));
+            },
+          );
+        });
+  }
+}
+
+class Child extends StatelessWidget {
+  final String pro;
+
+  const Child({Key key, @required this.pro}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('detail'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('返回 $pro'),
+          onPressed: () {
+            Navigator.of(context).pop('15890185113-$pro');
+          },
+        ),
+      ),
     );
   }
 }
