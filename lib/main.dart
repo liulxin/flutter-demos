eb21acf5d1f0b88f1e31d1373544a3bfe03cb395
liulxin <550293874@qq.com>
2020-03-30 15:26:27 +0800

    row column

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-    new MyApp(items: new List<String>.generate(10, (i) => 'this is item $i')));
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-
-  MyApp({Key key, @required this.items}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final title = 'Horizontal List';
@@ -18,45 +13,51 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(title),
         ),
-        body: new Center(
-          child: new Container(
-            height: 200.0,
-//            child: MyList(),
-            child: new ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return new ListTile(
-                  title: new Text('${items[index]}'),
-                );
-              },
+        body: new Row(
+          children: <Widget>[
+            Expanded(
+              child: new RaisedButton(
+                onPressed: () {},
+                color: Colors.amber,
+                child: new Text('button'),
+              ),
             ),
-          ),
+            new RaisedButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: new Text('button'),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new RaisedButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  child: new Text('button'),
+                ),
+                new RaisedButton(
+                  onPressed: () {},
+                  color: Colors.pink,
+                  child: new Text('button'),
+                ),
+                new RaisedButton(
+                  onPressed: () {},
+                  color: Colors.lightBlueAccent,
+                  child: new Text('button'),
+                )
+              ],
+            ),
+            Expanded(
+              child: new RaisedButton(
+                onPressed: () {},
+                color: Colors.lightGreen,
+                child: new Text('button'),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-//class MyList extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    // TODO: implement build
-//    return new ListView(
-//      scrollDirection: Axis.horizontal,
-//      children: <Widget>[
-//        new Container(
-//          width: 180.0,
-//          color: Colors.lightGreen,
-//        ),
-//        new Container(
-//          width: 180.0,
-//          color: Colors.deepPurple,
-//        ),
-//        new Container(
-//          width: 180.0,
-//          color: Colors.amber,
-//        )
-//      ],
-//    );
-//  }
-//}
