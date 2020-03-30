51f9ebfa9453d79698701f1418ae5ed761239a59
liulxin <550293874@qq.com>
2020-03-30 10:55:27 +0800

    Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var title = 'Web Images';
+    final title = 'Horizontal List';
 
     return new MaterialApp(
       title: title,
       home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text(title),
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Center(
+          child: Text('hello flutter hello flutter hello flutter hello flutter hello flutter hello flutter hello flutter',
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 14.0,
+            color: Colors.pink,
+            decoration: TextDecoration.underline,
+            decorationStyle: TextDecorationStyle.dashed
           ),
-          body: new Column(
-            children: <Widget>[
-              // 静态图
-              new Image.network('https://static.web.sdo.com/gmys/20190219_web/g.png'),
-              // gif
-              new Image.network('https://static.web.sdo.com/jijiamobile/pic/gmysweb6/pc/play.gif'),
-              // 占位符淡入图片
-              new FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image: 'https://static.web.sdo.com/gmys/20190219_web/g.png',
-              ),
-              // 缓存图片
-              new CachedNetworkImage(
-                  imageUrl: 'https://static.web.sdo.com/gmys/20190219_web/g.png',
-                  placeholder: (context, url) => new CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => Icon(Icons.error)
-              ),
-            ],
-          )),
+          )
+        )
+      ),
     );
   }
 }
